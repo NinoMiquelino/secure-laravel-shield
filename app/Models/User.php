d5865197f32f12fd955e786fb546e7dce3514542
Onivaldo Miquelino <ninomiquelino@gmail.com>
2025-11-10 20:01:47 -0300

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'ip' => request()->ip(),
+            'user_agent' => request()->userAgent(),
+            'jti' => uniqid()
+        ];
+    }
+
+    /**
+     * Check if user has suspicious activity
+     */
+    public function hasSuspiciousActivity()
+    {
+        // Implementar lógica de detecção de atividades suspeitas
+        return false;
+    }
+}
